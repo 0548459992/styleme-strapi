28361580c8cc8f0ac18964f043687310803decc2
unknown <chaimyabrov@gmail.com>
2025-04-21 23:45:51 +0300

    fix: add APP_KEYS once and update server.js

--- config/server.ts
+++ config/server.ts
@@ -2,4 +2,5 @@ export default ({ env }) => ({
   url: env('RENDER_EXTERNAL_URL'), // זה מה שחשוב
   host: '0.0.0.0',
   port: env.int('PORT', 1337),
+  keys: env.array('APP_KEYS'), // 👈 מוסיפים את זה
 });
