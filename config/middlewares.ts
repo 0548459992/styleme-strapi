6e19d62609ad8120d3cbca769fea4d39a9948072
unknown <chaimyabrov@gmail.com>
2025-04-21 14:10:46 +0300

    update CORS origins

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,15 +1,21 @@
-module.exports = [
-  'strapi::logger',
+export default [
   'strapi::errors',
-  'strapi::security',
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:3000'], // כתובת הלקוח שלך
-      headers: '*', // אפשר לכל הכותרות לעבור
+      origin: [
+        'http://localhost:3000', // פיתוח
+        'https://styleme.fashion', // האתר הראשי בפרודקשן
+        'https://blog.styleme.fashion', // תת הדומיין לפרונט של הבלוג
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      headers: ['Content-Type', 'Authorization'],
+      credentials: true,
     },
   },
+  'strapi::security',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,5 @@
-export default ({ env }: { env: any }) => ({
-  host: env('HOST', '0.0.0.0'),
+export default ({ env }) => ({
+  url: env('RENDER_EXTERNAL_URL'), // זה מה שחשוב
+  host: '0.0.0.0',
   port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
 });
