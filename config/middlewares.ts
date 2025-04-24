bc5543fe78c0d2f4b909d34fadc556e45d80551a
unknown <chaimyabrov@gmail.com>
2025-04-24 10:10:52 +0300

    🔧 עדכון CORS ב-Strapi לתמיכה בדומיין של הבלוג

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,19 +1,19 @@
 module.exports = [
   'strapi::errors',
+  'strapi::security',
   {
     name: 'strapi::cors',
     config: {
       origin: [
         'https://styleme-blog-pichuovy7-chaim-yabrovs-projects.vercel.app',
         'https://blog.styleme.fashion',
-        'http://localhost:3000' // לשימוש בפיתוח
+        'http://localhost:3000'
       ],
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'HEAD'],
-      headers: '*',
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       credentials: true,
     },
   },
-  'strapi::security',
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
