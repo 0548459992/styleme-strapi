ab4c6ddeba924923ae7e8a773fb61bb033072d27
unknown <chaimyabrov@gmail.com>
2025-04-24 12:21:05 +0300

    Fix CORS placement and headers

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,20 +1,26 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
   {
     name: 'strapi::cors',
     config: {
       origin: [
-        'https://styleme-blog-pichuovy7-chaim-yabrovs-projects.vercel.app',
         'https://blog.styleme.fashion',
+        'https://styleme-blog-pichuovy7-chaim-yabrovs-projects.vercel.app',
         'http://localhost:3000'
       ],
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'HEAD'],
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept', 'X-Requested-With'],
+      headers: [
+        'Content-Type',
+        'Authorization',
+        'Origin',
+        'Accept',
+        'X-Requested-With'
+      ],
       credentials: true,
       keepHeaderOnError: true
     },
   },
+  'strapi::security',
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
