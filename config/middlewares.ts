305df8ee97a4a41e2e8cb07accf48b6bfdf4834a
unknown <chaimyabrov@gmail.com>
2025-04-28 11:42:58 +0300

    fix: update middlewares.ts to allow CORS + general improvements

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,25 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'script-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https:'],
+          'connect-src': ["'self'", 'https:'],
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['*'], // או שתרשום פה את הדומיין שלך במקום '*'
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
