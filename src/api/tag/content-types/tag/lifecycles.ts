fcd7fcf4b7c821d2831921a76110127700d1d3a0
unknown <chaimyabrov@gmail.com>
2025-05-01 20:56:45 +0300

    📦 add jins slug to static paths

--- src/api/tag/content-types/tag/lifecycles.ts
+++ src/api/tag/content-types/tag/lifecycles.ts
@@ -1,21 +1,19 @@
 // src/api/tag/content-types/tag/lifecycles.ts
 
-import slugifyImport from 'slugify';
-
-const slugify = slugifyImport as (str: string, options?: { lower?: boolean; strict?: boolean }) => string;
+import slugify from 'slugify';
 
 export default {
   async beforeCreate(event) {
     const { data } = event.params;
     if (data.name && !data.slug) {
-      data.slug = slugify(data.name, { lower: true, strict: true });
+      data.slug = slugify(data.name, { lower: true });
     }
   },
 
   async beforeUpdate(event) {
     const { data } = event.params;
     if (data.name && !data.slug) {
-      data.slug = slugify(data.name, { lower: true, strict: true });
+      data.slug = slugify(data.name, { lower: true });
     }
   },
 };
